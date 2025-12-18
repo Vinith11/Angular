8cce0d4663529da4c89939df528f1a4d4a265056
Vinith11 <vinith2003@gmail.com>
2025-12-18 11:14:03 +0530

    initialize

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  title1 = signal('My ');
+  title2 = 'Application';
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  isDisabled = true;
+
+  handleClick() {
+  console.log('Button clicked');
+}
+
+}
+
+
